5c3b6c401d64662d83bd6c83d52754ba7e005122
Dewa Putra Hernanda <dewananda089@gmail.com>
2022-12-25 15:55:31 +0700

    validasi text form

--- app/src/main/java/com/example/ketapp/Login.java
+++ app/src/main/java/com/example/ketapp/Login.java
@@ -4,19 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class Login extends AppCompatActivity {
+
+    EditText textUsername;
+    EditText textPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //        Binding Text Username
+        textUsername = findViewById(R.id.txt_username);
+        textPassword = findViewById(R.id.txt_password);
     }
 
-    public void clickLoginAccount(View view){
-        Intent i = new Intent(Login.this, dashboard.class);
-        startActivity(i);
+    public void clickLoginAccount(View view) {
+        if (TextUtils.isEmpty(textUsername.getText().toString().trim()) || TextUtils.isEmpty(textPassword.getText().toString().trim())) {
+            Toast.makeText(view.getContext(), "Username tidak boleh kosong!", Toast.LENGTH_LONG).show();
+        } else {
+            Intent i = new Intent(Login.this, dashboard.class);
+            startActivity(i);
+        }
     }
 
     public void clickCreateAccount(View view){
